298e9872a29dafa569addffd327132d05478484f
Enock Cheruiyot Korir <enockorir123@gmai.com>
2025-03-26 07:59:09 +0300

    simple liker

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
-// Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const modal = document.getElementById('modal');
+  const modalMessage = document.getElementById('modal-message');
+  modal.classList.add('hidden'); // Make sure the modal is hidden on page load
 
+  const hearts = document.querySelectorAll('.like-glyph');
+
+  hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          if (heart.textContent === EMPTY_HEART) {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          } else {
+            heart.textContent = EMPTY_HEART;
+            heart.classList.remove('activated-heart');
+          }
+        })
+        .catch(error => {
+          modal.classList.remove('hidden');
+          modalMessage.textContent = error;
+          setTimeout(() => {
+            modal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
